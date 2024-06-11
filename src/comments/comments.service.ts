61966c54576740ffd5671c45994fa2f7df440ad4
vitor100110 <231037048@aluno.unb.br>
2024-06-11 19:32:50 -0300

    :bugfix: nao tava dando pull, corrigi

--- src/comments/comments.service.ts
+++ src/comments/comments.service.ts
@@ -5,55 +5,51 @@ import { PrismaService } from 'src/prisma.service';
 
 @Injectable()
 export class CommentsService {
-  constructor(private prismaService: PrismaService) {
-
-  }
+  constructor(private prismaService: PrismaService) {}
   async create(createCommentDto: CreateCommentDto) {
-    return await this.prismaService.comentarios.create({
+    return await this.prismaService.comment.create({
       data: createCommentDto,
     });
   }
 
   async findAll() {
-    return await this.prismaService.comentarios.findMany();
+    return await this.prismaService.comment.findMany();
   }
 
   async findOne(id: number) {
-    const ValidarId = await this.prismaService.comentarios.findUnique({
-      where: { id }
+    const ValidarId = await this.prismaService.comment.findUnique({
+      where: { id },
     });
-    if(!ValidarId){
-      throw new NotFoundException ("Usuario Invalido")
+    if (!ValidarId) {
+      throw new NotFoundException('Usuario Invalido');
     }
-    return this.prismaService.comentarios.findUnique({
-      where: { id }
+    return this.prismaService.comment.findUnique({
+      where: { id },
     });
   }
 
   async update(id: number, updateCommentDto: UpdateCommentDto) {
-    const ValidarId = await this.prismaService.comentarios.findUnique({
-      where: { id }
+    const ValidarId = await this.prismaService.comment.findUnique({
+      where: { id },
     });
-    if(!ValidarId){
-      throw new NotFoundException ("Usuario Invalido")
+    if (!ValidarId) {
+      throw new NotFoundException('Usuario Invalido');
     }
-    return await this.prismaService.comentarios.update({
+    return await this.prismaService.comment.update({
       where: { id },
       data: updateCommentDto,
-
     });
   }
 
   async remove(id: number) {
-    const ValidarId = await this.prismaService.comentarios.findUnique({
-      where: { id },
-    })
-    if(!ValidarId){ 
-    throw new NotFoundException ("Usuario invalido")
-    }
-    return await this.prismaService.comentarios.delete( {
+    const ValidarId = await this.prismaService.comment.findUnique({
       where: { id },
     });
-  
+    if (!ValidarId) {
+      throw new NotFoundException('Usuario invalido');
+    }
+    return await this.prismaService.comment.delete({
+      where: { id },
+    });
   }
 }
