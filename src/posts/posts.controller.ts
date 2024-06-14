2ff17c438b9300f32f161f81c19b34949cb47cd6
Billian <willianppl1958@gmail.com>
2024-06-14 18:04:20 -0300

    :sparkles: feat: CurrentUser, verificar se o quem está fazendo alteração e o dono do id, e feito no posts

--- src/posts/posts.controller.ts
+++ src/posts/posts.controller.ts
@@ -1,34 +1,47 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, UnauthorizedException } from '@nestjs/common';
 import { PostsService } from './posts.service';
 import { CreatePostDto } from './dto/create-post.dto';
 import { UpdatePostDto } from './dto/update-post.dto';
+import { UserPayload } from 'src/auth/types/UserPayload';
+import { CurrentUser } from 'src/auth/decorators/current-user.deciratirs';
 
-@Controller('posts')
-export class PostsController {
+@Controller('post')
+export class PostController {
   constructor(private readonly postsService: PostsService) {}
 
   @Post()
-  create(@Body() createPostDto: CreatePostDto) {
+  async create(@Body() createPostDto: CreatePostDto, @CurrentUser() currentUser: UserPayload) {
+    if (createPostDto.usuarioID !== currentUser.sub) {
+      throw new UnauthorizedException('Só é possível criar posts para si mesmo');
+    }
     return this.postsService.create(createPostDto);
   }
 
   @Get()
-  findAll() {
+  async findAll() {
     return this.postsService.findAll();
   }
 
   @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.postsService.findOne(+id);
+  async findOne(@Param('id') id: number) {
+    return this.postsService.findOne(id);
   }
 
   @Patch(':id')
-  update(@Param('id') id: string, @Body() updatePostDto: UpdatePostDto) {
-    return this.postsService.update(+id, updatePostDto);
+  async update(@Param('id') id: number, @Body() updatePostDto: UpdatePostDto, @CurrentUser() currentUser: UserPayload) {
+    const post = await this.postsService.findOne(id);
+    if (post.usuarioID !== currentUser.sub) {
+      throw new UnauthorizedException('Você só pode atualizar seus próprios posts');
+    }
+    return this.postsService.update(id, updatePostDto);
   }
 
   @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.postsService.remove(+id);
+  async remove(@Param('id') id: number, @CurrentUser() currentUser: UserPayload) {
+    const post = await this.postsService.findOne(id);
+    if (post.usuarioID !== currentUser.sub) {
+      throw new UnauthorizedException('Você só pode deletar seus próprios posts');
+    }
+    return this.postsService.remove(id);
   }
 }
